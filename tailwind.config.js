591ea74b1224eaf98835e5933c440ff22d58f231
Роман Нарожнов <rommka_83@mail.ru>
2023-10-21 15:00:25 +0300

    settings corrected

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,7 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 
 /** @type {import('tailwindcss').Config} */
 export default withMT({
-  // purge: ['./src/**/*.tsx'], настройка оптимизации
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
